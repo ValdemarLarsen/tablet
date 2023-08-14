119e75d6ea48c83f622d3d6ac399de71c472b849
Valdemar Larsen <41428728+ValdemarLarsen@users.noreply.github.com>
2023-08-14 21:50:48 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
+const {nextui} = require("@nextui-org/react");
+
+
 /** @type {import('tailwindcss').Config} */ 
 module.exports = {
     content: [
       "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
+      "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     ],
     darkMode: 'class',
     theme: {
       extend: {},
     },
     // add daisyUI plugin
-    plugins: [require("daisyui")],
+    plugins: [require("daisyui"), nextui()],
     daisyui: {
       styled: true,
       themes: true,
