abbf59ea34e61b8977054b173863787e13067828
Valdemar Larsen <41428728+ValdemarLarsen@users.noreply.github.com>
2023-08-14 22:45:38 +0200

    Update Navbar.tsx

--- pages/comp/Navbar.tsx
+++ pages/comp/Navbar.tsx
@@ -9,15 +9,15 @@ const Navbar = () => {
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
 
-                    <div className="dropdown dropdown-bottom">
+                    <div className="dropdown dropdown-bottom text-white">
                         <label tabIndex={0} className="btn m-1">Kriminlaregister</label>
-                        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2">
                             <li><a>Søg i kr</a></li>
                             <li><a>Opret til kr</a></li>
                         </ul>
                     </div>
 
-                    <li><a><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                    <li><a className="text-white"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-geo-alt-fill" viewBox="0 0 16 16">
                         <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
                     </svg>Flådestyring</a></li>
                 </ul>
