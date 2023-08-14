9ecdefc1f564310922ea76cc584fb2179f399039
Valdemar Larsen <41428728+ValdemarLarsen@users.noreply.github.com>
2023-08-15 01:52:37 +0200

    updates

---
+++ pages/forside/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+import Navbar from '../comp/Navbar';
+
+const Om: NextPage = () => {
+  return (
+      
+    <div>
+      <Navbar />  
+    </div>
+  );
+}
+
+export default Om;
