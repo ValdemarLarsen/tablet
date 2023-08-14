7b1fab5e75901f50b8e7c3e6daff2dad9aaa32c3
Valdemar Larsen <41428728+ValdemarLarsen@users.noreply.github.com>
2023-08-14 18:09:30 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ const HomepageContent = () => { // TO AVOID https://i.imgur.com/798mUI4.png (FOR
         e.preventDefault(); // Forhindrer formen i at genindlæse siden
 
         sendNuiCallback("/indhenterBrugere", { email, password }, (result) => {
-            console.log(JSON.parse(result));
+            console.log(result.bolean);
         });
     }
 
