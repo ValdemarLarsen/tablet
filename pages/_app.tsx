7a7b586e41083dc91976e310fd4a7efa7387cc69
Valdemar Larsen <41428728+ValdemarLarsen@users.noreply.github.com>
2023-08-14 18:25:12 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,12 @@
 import { ThemeProvider } from 'next-themes'
 import '../styles/globals.css'
-import Layout from './layout';
-
 
 const MyApp = ({ Component, pageProps }) => (
-    <Layout>
-        <ThemeProvider attribute="class" enableColorScheme={false}>
-            <Component {...pageProps} />
-        </ThemeProvider>
-    </Layout>
+
+    <ThemeProvider attribute="class" enableColorScheme={false}>
+        <Component {...pageProps} />
+    </ThemeProvider>
+
 )
 
 export default MyApp
