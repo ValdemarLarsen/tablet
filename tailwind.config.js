284c00a8c4f3b7d7ba8444bb0b87bc89097d95d7
Valdemar Larsen <41428728+ValdemarLarsen@users.noreply.github.com>
2023-08-14 22:13:42 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,5 @@ module.exports = {
       extend: {},
     },
     // add daisyUI plugin
-    plugins: [require("daisyui")],
-    daisyui: {
-      styled: true,
-      themes: true,
-      base: true,
-      utils: true,
-      logs: true,
-      rtl: false,
-  },
+    plugins: [require("daisyui")]
 }
