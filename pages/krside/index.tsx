4c455cf7464866f5aeeee95f46aac26e0f63bce4
Valdemar Larsen <41428728+ValdemarLarsen@users.noreply.github.com>
2023-08-15 01:41:02 +0200

    Update index.tsx

--- pages/krside/index.tsx
+++ pages/krside/index.tsx
@@ -6,7 +6,7 @@ export default function ProductDetailsWithInfoSidebar() {
                 <div className="container xl:max-w-7xl mx-auto px-4 py-16 lg:px-8 lg:py-32">
                     {/* Heading */}   <h2 className="flex flex-col sm:flex-row sm:items-end sm:space-x-2 text-2xl font-bold py-2 border-b-2 border-gray-200 mb-4 lg:mb-8 dark:border-gray-700">
                         <span className="inline-flex items-center space-x-2">
-                            <svg className="hi-mini hi-paper-clip inline-block w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"><path fill-rule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clip-rule="evenodd" /></svg>
+                            <svg className="hi-mini hi-paper-clip inline-block w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"><path fillRule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clipRule="evenodd" /></svg>
                             <span>Kriminalregister</span>
                         </span>
                         <span className="font-medium text-lg text-gray-600 dark:text-gray-400">| Valdemar Larsen</span>
