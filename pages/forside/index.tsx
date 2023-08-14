8d28987508876e7f93dc118158c37ed107b8b49a
Valdemar Larsen <41428728+ValdemarLarsen@users.noreply.github.com>
2023-08-14 22:02:20 +0200

    Update index.tsx

--- pages/forside/index.tsx
+++ pages/forside/index.tsx
@@ -4,10 +4,7 @@ import Navbar from '../comp/Navbar';
 const Om: NextPage = () => {
   return (
     <div>
-      <div>
         <Navbar />
-        {/* Resten af siden */}
-      </div>
       <h1>Om os</h1>
       <p>Her kan du læse mere om vores firma og vores mission.</p>
     </div>
