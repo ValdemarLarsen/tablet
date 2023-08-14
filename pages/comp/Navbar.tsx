4aba9a92646cf7f6c863bf834f2bc821fdeb7a4d
Valdemar Larsen <41428728+ValdemarLarsen@users.noreply.github.com>
2023-08-15 01:57:21 +0200

    Update Navbar.tsx

--- pages/comp/Navbar.tsx
+++ pages/comp/Navbar.tsx
@@ -156,7 +156,7 @@ export default function Navbar() {
 
               <div className="flex items-center justify-center mb-3">
                 <svg className="w-6 h-6 text-gray-800 dark:text-white mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 18">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 12v5m5-9v9m5-5v5m5-9v9M1 7l5-6 5 6 5-6" />
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 12v5m5-9v9m5-5v5m5-9v9M1 7l5-6 5 6 5-6" />
                 </svg>
                 <h1 className="text-3xl font-bold">Statistikker for {currentMonth}</h1>
               </div>
