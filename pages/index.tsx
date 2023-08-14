00b7f92a2eb8ffc7388bf4cde03a6e2ea5d51db7
Valdemar Larsen <41428728+ValdemarLarsen@users.noreply.github.com>
2023-08-14 17:58:31 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import { useState } from 'react';
 import { Provider, useDispatch, useSelector } from "react-redux"
 import { setButtonClicked } from "../state/reducers/app"
 import store, { RootState } from "../state/store"
@@ -28,22 +29,29 @@ const HomepageContent = () => { // TO AVOID https://i.imgur.com/798mUI4.png (FOR
 
     const router = useRouter();
 
-    const handleClick = () => {
-        // Her antager jeg, at du ønsker at navigere til "/om"-siden.
-        router.push('/forside');
-        sendNuiCallback("/indhenterBrugere", {}, (result) => {})
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // Forhindrer formen i at genindlæse siden
+
+        sendNuiCallback("/indhenterBrugere", { email, password }, (result) => {
+            console.log(result);
+        });
     }
 
     return (
         <div className="flex flex-col justify-center items-center w-screen h-screen bg-opacity-50 bg-black">
             <div className="p-12 bg-gray-200 rounded-lg shadow-lg">
                 <h1 className="text-2xl font-bold mb-4">Log ind</h1>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-4">
                         <label htmlFor="email" className="block text-sm font-medium text-gray-600 mb-2">Email:</label>
                         <input
                             type="email"
                             id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                             className="w-full p-2 border rounded-md"
                             placeholder="Din email"
                         />
@@ -53,6 +61,8 @@ const HomepageContent = () => { // TO AVOID https://i.imgur.com/798mUI4.png (FOR
                         <input
                             type="password"
                             id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                             className="w-full p-2 border rounded-md"
                             placeholder="Dit kodeord"
                         />
@@ -60,7 +70,6 @@ const HomepageContent = () => { // TO AVOID https://i.imgur.com/798mUI4.png (FOR
                     <button
                         type="submit"
                         className="daisy-btn daisy-btn-primary"
-                        onClick={handleClick}
                     >
                         Log ind
                     </button>
