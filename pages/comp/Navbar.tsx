d7743933f3c28ae6ff7ac3fb94775813055dbd44
Valdemar Larsen <41428728+ValdemarLarsen@users.noreply.github.com>
2023-08-14 22:40:08 +0200

    Update Navbar.tsx

--- pages/comp/Navbar.tsx
+++ pages/comp/Navbar.tsx
@@ -4,17 +4,19 @@ const Navbar = () => {
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
-                <img src="https://media.discordapp.net/attachments/1008406017903706163/1131009951754629170/politi.png" className="w-20 h-10"/>
+                <img src="https://media.discordapp.net/attachments/1008406017903706163/1131009951754629170/politi.png" className="w-20 h-10" />
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
                     <li><a>Item 1</a></li>
-                    <li tabIndex={0}>
+                    <li>
                         <div className="dropdown dropdown-bottom">
-                            <label tabIndex={0} className="btn m-1">Click</label>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-archive-fill" viewBox="0 0 16 16">
+                                <path d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM5.5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z" />
+                            </svg><label tabIndex={0} className="btn m-1">Kriminlaregister</label>
                             <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                                <li><a>Item 1</a></li>
-                                <li><a>Item 2</a></li>
+                                <li><a>Søg i kr</a></li>
+                                <li><a>Opret til kr</a></li>
                             </ul>
                         </div>
                     </li>
