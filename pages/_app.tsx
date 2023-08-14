c2775cff33a2c96786f918a4176ec8a924c722e1
Valdemar Larsen <41428728+ValdemarLarsen@users.noreply.github.com>
2023-08-14 21:26:45 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,11 @@
-import { ThemeProvider } from "./themeprovider";
+import { ThemeProvider } from 'next-themes'
 import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }) => (
-    <html lang="en">
-        <body
-            className={`${inter.className} bg-slate-50 dark:bg-[#0d1117] ${switchThemeDuration}`}
-        >
-            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-                <ThemeSwitcher />
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </body>
-    </html>
-
+    
+    <ThemeProvider attribute="class" enableColorScheme={false}>
+        <Component {...pageProps} />
+    </ThemeProvider>
 )
 
 export default MyApp
