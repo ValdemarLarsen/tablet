d9fd40894b4bca9b6e3586ca3f81861bf049d685
Valdemar Larsen <41428728+ValdemarLarsen@users.noreply.github.com>
2023-08-15 02:47:11 +0200

    Dynamic tabs?

--- pages/forside/index.tsx
+++ pages/forside/index.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from 'next';
 import Navbar from '../comp/Navbar';
-
+import Tabs from  '../comp/Tabs';
 
 
 const Om: NextPage = () => {
   return (
       
     <div>
-      <Navbar />  
+      <Navbar />
+      <Tabs />  
     </div>
   );
 }
