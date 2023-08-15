ac0a738f39403436493ffff3c46b96f08a780d0a
Valdemar Larsen <41428728+ValdemarLarsen@users.noreply.github.com>
2023-08-15 16:45:26 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,9 +31,8 @@ const Homepage = () => (
 
 const HomepageContent = () => { // TO AVOID https://i.imgur.com/798mUI4.png (FOR DEMO PUPOSES ONLY)
     const dispatch = useDispatch()
-    const { systemTheme, theme, setTheme } = useTheme();
-    const currentTheme = theme === 'system' ? systemTheme : theme;
-    const buttonClicked = useSelector((state: RootState) => state.app.buttonClicked)
+
+   
 
 
     const router = useRouter();
@@ -64,11 +63,6 @@ const HomepageContent = () => { // TO AVOID https://i.imgur.com/798mUI4.png (FOR
         <div className="flex flex-col justify-center items-center w-screen h-screen bg-opacity-0 bg-black">
             <div className="p-12 bg-gray-200 rounded-lg shadow-lg">
                 <h1 className="text-2xl font-bold mb-4">Log ind</h1>
-                <button
-                    onClick={() => theme == "dark" ? setTheme('light') : setTheme("dark")}
-                    className='bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 px-8 py-2 text-2xl md:text-4xl rounded-lg absolute bottom-32'>
-                    Toggle Mode
-                </button>
                 <form onSubmit={handleSubmit}>
                     <div className="mb-4">
                         <label htmlFor="email" className="block text-sm font-medium text-gray-600 mb-2">Email:</label>
