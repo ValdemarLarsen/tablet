14eab5a2b7df8ed4e94185dbddcf5fa041de5482
Valdemar Larsen <41428728+ValdemarLarsen@users.noreply.github.com>
2023-08-15 17:02:24 +0200

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function TransparentPage() {
+    return (
+        <div style={{ height: "100vh", width: "100%", backgroundColor: 'rgba(255, 255, 255, 0.3)' }}>
+            This is a mostly transparent page.
+        </div>
+    )
+}
