538b44179f0a237ea88317cd105e1bca8486a2a2
Valdemar Larsen <41428728+ValdemarLarsen@users.noreply.github.com>
2023-08-15 17:07:36 +0200

    updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
-export default function TransparentPage() {
+import React from 'react';
+
+
+
+export default function Home() {
+    const [hasData, setHasData] = React.useState(false);
+
+    React.useEffect(() => {
+        // Her kan du lytte efter data fra din backend eller en hvilken som helst anden kilde.
+        // Som en simpel demonstration bruger jeg setTimeout for at efterligne det:
+        setTimeout(() => {
+            setHasData(true);
+        }, 5000); // Efter 5 sekunder vil indholdet blive synligt
+    }, []);
+
     return (
-        <div style={{ height: "100vh", width: "100%", backgroundColor: 'rgba(255, 255, 255, 0.3)' }}>
-            This is a mostly transparent page.
+        <div className={`h-screen w-full ${hasData ? 'bg-white' : 'bg-transparent'}`}>
+            {hasData ? "Her er dataen!" : null}
         </div>
-    )
+    );
 }
