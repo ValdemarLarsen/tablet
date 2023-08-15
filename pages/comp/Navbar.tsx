b7636fbd64e53046c18ae33ef469744700c0d46f
Valdemar Larsen <41428728+ValdemarLarsen@users.noreply.github.com>
2023-08-15 18:16:35 +0200

    Update Navbar.tsx

--- pages/comp/Navbar.tsx
+++ pages/comp/Navbar.tsx
@@ -161,107 +161,109 @@ export default function Navbar() {
 
               {/* Statistics: Simple with Action */}
               {/* Placeholder - Start */}
-              <div
-                className={'flex-none jutify-center lg:flex flex-col w-full lg:w-80 xl:w-96 p-4 lg:p-8 bg-gray-50 dark:bg-gray-800 dark:bg-opacity-25'}
-              >
-                <div className="flex-auto flex items-center justify-center rounded-xl bg-gray-50 border-2 border-dashed border-gray-200 text-gray-400 py-64 dark:bg-gray-800 dark:border-gray-700">Side Content</div>
-              </div>
-              {/* Placeholder - End */}
-              <div className="flex items-center justify-center mb-3">
-                <svg className="w-6 h-6 text-gray-800 dark:text-white mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 18">
-                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 12v5m5-9v9m5-5v5m5-9v9M1 7l5-6 5 6 5-6" />
-                </svg>
-                <h1 className="text-3xl font-bold">Statistikker for {currentMonth}</h1>
-              </div>
-
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-4 lg:gap-8">
-                {/* Card */}
-                <div className="flex flex-col rounded-lg shadow-sm bg-white overflow-hidden dark:text-gray-100 dark:bg-gray-800">
-                  {/* Body */}
-                  <div className="p-5 grow flex justify-between items-center">
-                    <dl className="space-y-1">
-                      <dt className="text-2xl font-bold">
-                        50
-                      </dt>
-                      <dd className="uppercase font-semibold text-sm text-gray-500 tracking-wider dark:text-gray-400">
-                        Sager Oprettet
-                      </dd>
-                    </dl>
-                    <div className="font-semibold inline-flex px-2 py-1 leading-4 items-center space-x-1 text-sm rounded-full text-emerald-800 bg-emerald-200">
-                      <svg className="hi-solid hi-arrow-circle-up inline-block w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z" clipRule="evenodd" /></svg>
-                      <span>7.9%</span>
-                    </div>
-                  </div>
-                  {/* END Body */}
-
-                  {/* Action Link */}
-                  <a href="#" className="block p-3 font-medium text-sm text-center bg-gray-50 hover:bg-gray-100 hover:bg-opacity-75 active:bg-gray-50 text-white dark:bg-gray-900/50 dark:hover:bg-gray-900/75 dark:active:bg-gray-900/50 font-bold">
-                    Søg efter sager
-                  </a>
-                  {/* END Action Link */}
+              <div className="grid grid-cols-3 gap-4 lg:gap-8">
+                <div
+                  className={'flex-none jutify-center lg:flex flex-col w-full lg:w-80 xl:w-96 p-4 lg:p-8 bg-gray-50 dark:bg-gray-800 dark:bg-opacity-25'}
+                >
+                  <div className="flex-auto flex items-center justify-center rounded-xl bg-gray-50 border-2 border-dashed border-gray-200 text-gray-400 py-64 dark:bg-gray-800 dark:border-gray-700">Side Content</div>
                 </div>
-                {/* END Card */}
-
-                {/* Card */}
-                <div className="flex flex-col rounded-lg shadow-sm bg-white overflow-hidden dark:text-gray-100 dark:bg-gray-800">
-                  {/* Body */}
-                  <div className="p-5 grow flex justify-between items-center">
-                    <dl className="space-y-1">
-                      <dt className="text-2xl font-bold">
-                        75.000 DKK
-                      </dt>
-                      <dd className="uppercase font-semibold text-sm text-gray-500 tracking-wider dark:text-gray-400">
-                        Sendt bøder for
-                      </dd>
-                    </dl>
-                    <div className="font-semibold inline-flex px-2 py-1 leading-4 items-center space-x-1 text-sm rounded-full text-rose-800 bg-rose-200">
-                      <svg className="hi-solid hi-arrow-circle-down inline-block w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z" clipRule="evenodd" /></svg>
-                      <span>6.9%</span>
-                    </div>
-                  </div>
-                  {/* END Body */}
-
-                  {/* Action Link */}
-                  <a href="#" className="block p-3 font-medium text-sm text-center bg-gray-50 hover:bg-gray-100 hover:bg-opacity-75 active:bg-gray-50 text-white dark:bg-gray-900/50 dark:hover:bg-gray-900/75 dark:active:bg-gray-900/50">
-                    Opret en ny sag
-                  </a>
-                  {/* END Action Link */}
+                {/* Placeholder - End */}
+                <div className="flex items-center justify-center mb-3">
+                  <svg className="w-6 h-6 text-gray-800 dark:text-white mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 18">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 12v5m5-9v9m5-5v5m5-9v9M1 7l5-6 5 6 5-6" />
+                  </svg>
+                  <h1 className="text-3xl font-bold">Statistikker for {currentMonth}</h1>
                 </div>
-                {/* END Card */}
 
-                {/* Card */}
-                <div className="flex flex-col rounded-lg shadow-sm bg-white overflow-hidden dark:text-gray-100 dark:bg-gray-800">
-                  {/* Body */}
-                  <div className="p-5 grow flex justify-between items-center">
-                    <dl className="space-y-1">
-                      <dt className="text-2xl font-bold">
-                        120 måneder
-                      </dt>
-                      <dd className="uppercase font-semibold text-sm text-gray-500 tracking-wider dark:text-gray-400">
-                        Samlet Fængseltid
-                      </dd>
-                    </dl>
-                    <div className="font-semibold inline-flex px-2 py-1 leading-4 items-center space-x-1 text-sm rounded-full text-emerald-800 bg-emerald-200">
-                      <svg className="hi-solid hi-arrow-circle-up inline-block w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z" clipRule="evenodd" /></svg>
-                      <span>5.6%</span>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 lg:gap-8">
+                  {/* Card */}
+                  <div className="flex flex-col rounded-lg shadow-sm bg-white overflow-hidden dark:text-gray-100 dark:bg-gray-800">
+                    {/* Body */}
+                    <div className="p-5 grow flex justify-between items-center">
+                      <dl className="space-y-1">
+                        <dt className="text-2xl font-bold">
+                          50
+                        </dt>
+                        <dd className="uppercase font-semibold text-sm text-gray-500 tracking-wider dark:text-gray-400">
+                          Sager Oprettet
+                        </dd>
+                      </dl>
+                      <div className="font-semibold inline-flex px-2 py-1 leading-4 items-center space-x-1 text-sm rounded-full text-emerald-800 bg-emerald-200">
+                        <svg className="hi-solid hi-arrow-circle-up inline-block w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z" clipRule="evenodd" /></svg>
+                        <span>7.9%</span>
+                      </div>
                     </div>
-                  </div>
-                  {/* END Body */}
+                    {/* END Body */}
 
-                  {/* Action Link */}
-                  <a href="#" className="block p-3 font-medium text-sm text-center bg-gray-50 hover:bg-gray-100 hover:bg-opacity-75 active:bg-gray-50 text-white dark:bg-gray-900/50 dark:hover:bg-gray-900/75 dark:active:bg-gray-900/50">
-                    Opret en ny sag
-                  </a>
-                  {/* END Action Link */}
+                    {/* Action Link */}
+                    <a href="#" className="block p-3 font-medium text-sm text-center bg-gray-50 hover:bg-gray-100 hover:bg-opacity-75 active:bg-gray-50 text-white dark:bg-gray-900/50 dark:hover:bg-gray-900/75 dark:active:bg-gray-900/50 font-bold">
+                      Søg efter sager
+                    </a>
+                    {/* END Action Link */}
+                  </div>
+                  {/* END Card */}
+
+                  {/* Card */}
+                  <div className="flex flex-col rounded-lg shadow-sm bg-white overflow-hidden dark:text-gray-100 dark:bg-gray-800">
+                    {/* Body */}
+                    <div className="p-5 grow flex justify-between items-center">
+                      <dl className="space-y-1">
+                        <dt className="text-2xl font-bold">
+                          75.000 DKK
+                        </dt>
+                        <dd className="uppercase font-semibold text-sm text-gray-500 tracking-wider dark:text-gray-400">
+                          Sendt bøder for
+                        </dd>
+                      </dl>
+                      <div className="font-semibold inline-flex px-2 py-1 leading-4 items-center space-x-1 text-sm rounded-full text-rose-800 bg-rose-200">
+                        <svg className="hi-solid hi-arrow-circle-down inline-block w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z" clipRule="evenodd" /></svg>
+                        <span>6.9%</span>
+                      </div>
+                    </div>
+                    {/* END Body */}
+
+                    {/* Action Link */}
+                    <a href="#" className="block p-3 font-medium text-sm text-center bg-gray-50 hover:bg-gray-100 hover:bg-opacity-75 active:bg-gray-50 text-white dark:bg-gray-900/50 dark:hover:bg-gray-900/75 dark:active:bg-gray-900/50">
+                      Opret en ny sag
+                    </a>
+                    {/* END Action Link */}
+                  </div>
+                  {/* END Card */}
+
+                  {/* Card */}
+                  <div className="flex flex-col rounded-lg shadow-sm bg-white overflow-hidden dark:text-gray-100 dark:bg-gray-800">
+                    {/* Body */}
+                    <div className="p-5 grow flex justify-between items-center">
+                      <dl className="space-y-1">
+                        <dt className="text-2xl font-bold">
+                          120 måneder
+                        </dt>
+                        <dd className="uppercase font-semibold text-sm text-gray-500 tracking-wider dark:text-gray-400">
+                          Samlet Fængseltid
+                        </dd>
+                      </dl>
+                      <div className="font-semibold inline-flex px-2 py-1 leading-4 items-center space-x-1 text-sm rounded-full text-emerald-800 bg-emerald-200">
+                        <svg className="hi-solid hi-arrow-circle-up inline-block w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z" clipRule="evenodd" /></svg>
+                        <span>5.6%</span>
+                      </div>
+                    </div>
+                    {/* END Body */}
+
+                    {/* Action Link */}
+                    <a href="#" className="block p-3 font-medium text-sm text-center bg-gray-50 hover:bg-gray-100 hover:bg-opacity-75 active:bg-gray-50 text-white dark:bg-gray-900/50 dark:hover:bg-gray-900/75 dark:active:bg-gray-900/50">
+                      Opret en ny sag
+                    </a>
+                    {/* END Action Link */}
+                  </div>
+                  {/* END Card */}
                 </div>
-                {/* END Card */}
-              </div>
-              {/* END Statistics: Simple with Action */}
-              {/* Placeholder - Start */}
-              <div
-                className={'flex-none jutify-center lg:flex flex-col w-full lg:w-80 xl:w-96 p-4 lg:p-8 bg-gray-50 dark:bg-gray-800 dark:bg-opacity-25'}
-              >
-                <div className="flex-auto flex items-center justify-center rounded-xl bg-gray-50 border-2 border-dashed border-gray-200 text-gray-400 py-64 dark:bg-gray-800 dark:border-gray-700">Side Content</div>
+                {/* END Statistics: Simple with Action */}
+                {/* Placeholder - Start */}
+                <div
+                  className={'flex-none jutify-center lg:flex flex-col w-full lg:w-80 xl:w-96 p-4 lg:p-8 bg-gray-50 dark:bg-gray-800 dark:bg-opacity-25'}
+                >
+                  <div className="flex-auto flex items-center justify-center rounded-xl bg-gray-50 border-2 border-dashed border-gray-200 text-gray-400 py-64 dark:bg-gray-800 dark:border-gray-700">Side Content</div>
+                </div>
               </div>
               {/* Placeholder - End */}
             </>
