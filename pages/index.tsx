29239d0ab501c83aef7a3a63356b1b572bf1bc75
Valdemar Larsen <41428728+ValdemarLarsen@users.noreply.github.com>
2023-08-15 17:10:18 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,11 @@ import React from 'react';
 
 
 export default function Home() {
-    const [hasData, setHasData] = React.useState(false);
 
-    React.useEffect(() => {
-        // Her kan du lytte efter data fra din backend eller en hvilken som helst anden kilde.
-        // Som en simpel demonstration bruger jeg setTimeout for at efterligne det:
-        setTimeout(() => {
-            setHasData(true);
-        }, 5000); // Efter 5 sekunder vil indholdet blive synligt
-    }, []);
 
     return (
-        <div className={`h-screen w-full ${hasData ? 'bg-white' : 'bg-transparent'}`}>
-            {hasData ? "Her er dataen!" : null}
+        <div className={'h-screen w-full bg-transparent'}>
+            
         </div>
     );
 }
